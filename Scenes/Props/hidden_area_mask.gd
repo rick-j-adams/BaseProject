e4482f4893c887b480052a8b4c048393cda1ef03
rick-j-adams <rickadams@hotmail.com>
2026-08-30 21:00:01 +1200

    added hidden area

---
+++ Scenes/Props/hidden_area_mask.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+@onready var sprite :Sprite2D = $Sprite2D
+
+var goodyBoxes = []
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	
+	if body is Dydimo:	
+		sprite.visible=true	
+	
+
+func removeAllGoodyBoxes()->void:
+	for gb in goodyBoxes:
+		if gb != null:
+			gb.destroy()
+	goodyBoxes.clear()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Dydimo:	
+		sprite.visible=false	
+		removeAllGoodyBoxes()
+	if body is CharacterBody2D:	
+		var parent = body.get_parent()
+		if parent!=null:
+			if parent is GoodyBox:
+				goodyBoxes.append(parent)
